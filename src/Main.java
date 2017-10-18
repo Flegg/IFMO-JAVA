19a61f4a7b52dce3c011e12cfabab8b4c7b83115
asiverskii <a.siverskii@gmail.com>
2017-10-18 12:21:06 +0300

    Arrays task 2&3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,33 @@
-import StringsArrays.Task1;
+import StringsArrays.*;
 
 public class Main {
     public static void main(String[] args) {
         //МАССИВЫ
 
         //Задание 1
-        int array[] = new int[10];
-        Task1.arrayInitialize(array);
-        Task1.arrayPrinting(array);
+        System.out.println("~~~TASK1~~~");
+
+        int array1[] = new int[10];
+        Task1.arrayInitialize(array1);
+        Task1.arrayPrinting(array1);
+
+        //Задание 2
+        System.out.println();
+        System.out.println("~~~TASK2~~~");
+
+        int array2[] = new int[50];
+        Task2.arrayInitialize(array2);
+        Task2.arrayPrinting(array2);
+
+        //Задание 3
+        System.out.println();
+        System.out.println("~~~TASK3~~~");
+
+        int array3[] = new int[15];
+        Task3.arrayInitialize(array3);
+        Task3.arrayPrinting(array3);
+        Task3.countOfEvenMum(array3);
+
+
     }
 }
