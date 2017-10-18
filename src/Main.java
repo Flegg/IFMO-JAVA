c96b0d564d37bfa71295fa2905d255ba95767ea3
asiverskii <a.siverskii@gmail.com>
2017-10-18 11:11:36 +0300

    Arrays 1 task

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import StringsArrays.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        //МАССИВЫ
+
+        //Задание 1
+        int array[] = new int[10];
+        Task1.arrayInitialize(array);
+        Task1.arrayPrinting(array);
+    }
+}
---
+++ src/StringsArrays/Task1.java
@@ -0,0 +1,23 @@
+package StringsArrays;
+
+public class Task1 {
+    private static final int MAX = 20;
+    public static void arrayInitialize(int[] arr) {
+        int i = 0;
+        int b = 0;
+
+        while(++b < MAX) {
+            arr[i++] = ++b;
+        }
+    }
+    public static void arrayPrinting(int[] arr) {
+        // в строчку
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        // в столбик
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("\n" + arr[i]);
+        }
+    }
+}
